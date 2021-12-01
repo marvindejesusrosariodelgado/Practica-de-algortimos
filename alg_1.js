125c237022978a3b80efef86b426924cd15e849d
marvindejesusrosariodelgado <rosariomalvin20@gmail.com>
2021-12-01 11:50:03 -0400

    practica numero 1 de algoritmos

---
+++ alg_1.js
@@ -0,0 +1,11 @@
+//Start
+let nombre = prompt(`Introduzca su nombre`)*1;
+let num = prompt(`Introduzca el numero de veces`);
+
+
+//Clicle
+for(let i=0; i < num; i++){
+    //Imprimir nom
+    console.log(nombre);
+}
+//end 
