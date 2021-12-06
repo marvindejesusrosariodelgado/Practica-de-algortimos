e69213edd37c4277b317ecccfb2862bdec96e28a
marvindejesusrosariodelgado <rosariomalvin20@gmail.com>
2021-12-06 10:06:55 -0400

    algoritmo numero 3

---
+++ alg_9.js
@@ -0,0 +1,21 @@
+// pronlema #9
+
+function suMax (arr, k) {
+    
+    let sum = 0;
+    let max = 0; 
+    let start = 0;
+    for (let end = 0; end < arr.length; end++) {
+        sum += arr[end];
+        if (end >= k-1) {
+            max = Math.max(max, sum);
+            sum -= arr[start];
+            start++;
+            
+        }
+        
+    }
+
+    return max;
+}
+console.log(suMax([2,1,5,1,3,2], 3))
