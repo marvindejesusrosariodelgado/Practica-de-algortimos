f5d5302ed9631a0b62f536a55cc4bb2d8528031a
marvindejesusrosariodelgado <rosariomalvin20@gmail.com>
2021-12-08 12:10:42 -0400

    commit de ejerciccio numero 15 nuevo

--- alg_15.js
+++ alg_15.js
@@ -1,16 +1,19 @@
 // promedio la suma de todos dividido en la cantidad de d
 let a = prompt(`digite un numero 1`)*1;
-let b = prompt(`digite un numero 1`)*1;
-let c = prompt(`digite un numero 1`)*1;
-let d = prompt(`digite un numero 1`)*1;
-let e = prompt(`digite un numero 1`)*1;
-let f = prompt(`digite un numero 1`)*1;
-let h = prompt(`digite un numero 1`)*1;
-let i = prompt(`digite un numero 1`)*1;
-let j = prompt(`digite un numero 1`)*1;
-let j = prompt(`digite un numero 1`)*1;
+let b = prompt(`digite un numero 2`)*1;
+let c = prompt(`digite un numero 3`)*1;
+let d = prompt(`digite un numero 4`)*1;
+let e = prompt(`digite un numero 5`)*1;
+let f = prompt(`digite un numero 6`)*1;
+let g = prompt(`digite un numero 7`)*1;
+let h = prompt(`digite un numero 8`)*1;
+let i = prompt(`digite un numero 9`)*1;
+let j = prompt(`digite un numero 10`)*1;
 
 
-sum = (a+b+c+d+e+f+g+h+i+j)/
+
+
+sum = (a+b+c+d+e+f+g+h+i+j)/10;
+
 
 console.log(sum);
