8cfc59c9e1e0db7c81d77cbc4f25a4e11726bcc1
marvindejesusrosariodelgado <rosariomalvin20@gmail.com>
2021-12-10 07:55:59 -0400

    alg_16

---
+++ alg_16.js
@@ -0,0 +1,11 @@
+// algortimo numero 16
+
+// variables
+
+let horas = prompt(`Inserte el numero de horas por semana`)*1;
+let salarioPorHora = prompt(`inserte su salario por hora`)*1;
+
+// operaciones 
+
+let salarioBruto = horas*salarioPorHora;
+
