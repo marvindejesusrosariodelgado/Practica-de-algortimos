181beadc013fcbea2fa309e54cf593d871debdae
marvindejesusrosariodelgado <rosariomalvin20@gmail.com>
2021-12-11 00:08:16 -0400

    algortimo numero 10 cambios preeliminares

---
+++ alg_10.js
@@ -0,0 +1,14 @@
+let x = 0;
+let part1 = 0;
+let part2 = 0;
+
+
+function ecquad(a, b, c) {
+part2 = Math.sqrt(Math.pow(b, 2)-4*(a*c));
+x = (-(b))+(part2)/2;
+console.log(x);
+
+}
+
+
+ecquad(3,6,7);
