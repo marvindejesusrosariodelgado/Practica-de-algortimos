20d2c085d29df32447a113f5ab3467c283e8dcec
marvindejesusrosariodelgado <rosariomalvin20@gmail.com>
2021-12-12 16:04:14 -0400

    algortimo numero 10 resuelto

--- alg_10.js
+++ alg_10.js
@@ -1,13 +1,19 @@
-let x = 0;
 
 function ecquad(a, b, c) {
 let part1= -b;
 let part2 = Math.pow(b, 2);
 let part3 = (part2)+(-4*(a*c));
-let part4 = (part1)+(Math.sqrt(part3));
-x = (part4)/(2*a);
-return x;
+if (part3 < 0) {
+    alert(`No se puede sacar la raiz de un numero negativo, introduzca otra secuencia de numeros`)
+   return ecquad;
 }
 
-ecquad(2,3,4);
+let part4 = (part1)+(Math.sqrt(part3));
+let x = (part4)/(2*a);
+
+console.log(x);
+
+}
+
+ecquad(5, -5, -6);
 
