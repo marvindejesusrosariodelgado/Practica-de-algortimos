11e7b673333e372228459aad27ff477b6fbf8afe
marvindejesusrosariodelgado <rosariomalvin20@gmail.com>
2021-12-14 11:40:28 -0400

    algoritmo 18

--- alg_18.js
+++ alg_18.js
@@ -2,12 +2,37 @@
 
 
 function tripoTriang(a, b, c) {
+  
 
-if (a == b && ) {
+if (a == b && b == c) {
+   if (a == c) {
+    console.log(`El triangulo es Equilatero`);
     
-}
+   } else{
+       console.log(`El triangulo es escaleno`);
+   }
+         }
 
+         if (a == b) {
+             console.log(`El triangulo es isosceles`);
+             
+         } else{ 
+             if (b == c) {
+                console.log(`El triangulo es isosceles`);
+                 
+             }
+             if (a == c) {
+                console.log(`El triangulo es isosceles`);
+                 
+             }
 
+         
+            }
+         }
 
-
+tripoTriang(5, 6, 7);
+if (condition) {
+    
+} else {
+    
 }
