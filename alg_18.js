f1af1869799479c69d5db7049d80b1f680a40df2
marvindejesusrosariodelgado <rosariomalvin20@gmail.com>
2021-12-15 11:12:59 -0400

    algortimo 18 solucion

--- alg_18.js
+++ alg_18.js
@@ -1,61 +1,33 @@
-//
 
 
 function tripoTriang(a, b, c) {
-  if (a == b && b == c) {
-     if (a == c) {
-      console.log(`El triangulo es Equilatero`);
-     }
-     
-  } else { 
-    if (a==b) {
-      console.log(`El triangulo es isosceles`);
-    } else {
-       if (b == c){
-         console.log(`El triangulo es isosceles`);
-          
-       } else {
-          if (a == c) {
-            console.log(`El triangulo es isosceles`);
-             
-          }
-       }
-       
-    }
-        
-     }
-     
-     console.log(`El triangulo es escaleno`);
-     
-    
-  }
 
-
-   /*
-if (a == b && b == c) {
-   if (a == c) {
-    console.log(`El triangulo es Equilatero`);
-    
-   } else{
-       console.log(`El triangulo es escaleno`);
-   }
-         }
-
-         if (a == b) {
+   if (a == b && b == c) {
+      if (a == c) {
+       console.log(`El triangulo es Equilatero`);
+      }
+ 
+   } else { 
+     if (a==b) {
+       console.log(`El triangulo es isosceles`);
+     } else {
+        if (b == c){
+          console.log(`El triangulo es isosceles`);
+ 
+        } else {
+           if (a == c) {
              console.log(`El triangulo es isosceles`);
-             
-         } else{ 
-             if (b == c) {
-                console.log(`El triangulo es isosceles`);
-                 
-             }
-             if (a == c) {
-                console.log(`El triangulo es isosceles`);
-                 
-             }
+ 
+           }
+        }
+ 
+     }
+ 
+      }
+ 
+ 
+ 
+   }
+ 
 
-         
-            }
-         }
-*/
-tripoTriang(6, 5, 7);
+tripoTriang(8, 8, 8);
