ff2d0e971a755ea30080c0a0ee5c91c78afe3a08
marvindejesusrosariodelgado <rosariomalvin20@gmail.com>
2021-12-15 11:38:09 -0400

    algortimo 21

--- alg_21.js
+++ alg_21.js
@@ -1,10 +1,24 @@
 // algoritmo 21
+/* Dada una matriz de números ordenados y una suma objetivo, busque un par en la matriz
+cuya suma sea igual al objetivo dado. Escriba una función para devolver los índices de los dos
+números (es decir, el par) de manera que se sumen al objetivo dado.
+Input: [1, 2, 3, 4, 6], sum=6
+Output: [1, 3]
+Los números en los índices 1 y 3 suman 6: 2+4=6 */
 
-function sumObj(arr, sumObjetivo) {
-    let arr.legth= 
+var matriz = [1, 2, 3, 4, 6];
+var sum = 6;
+function searchPair(matriz, sum) {
+    var i = 0;
+    var j = matriz.length - 1;
+    while (i < j) {
+        if (matriz[i] + matriz[j] == sum) {
+            return [i, j];
+        } else if (matriz[i] + matriz[j] < sum) {
+            i++;
+        } else {
+            j--;
+        }
+    }
 }
-
-
-
-
-sumObj([1,2,3,4,6], 6);
+console.log(searchPair(matriz, sum));
