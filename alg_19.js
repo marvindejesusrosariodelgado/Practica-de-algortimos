6507149722f57e2388b4d237c8009bba488897c8
marvindejesusrosariodelgado <rosariomalvin20@gmail.com>
2021-12-15 11:12:35 -0400

    algortimo 19 solucion

---
+++ alg_19.js
@@ -0,0 +1,16 @@
+// algoritmo 19
+// Variables 
+var b = 2;
+var c = 3;
+
+for(var a = 0; a<= 5; ++a){
+    for(var i = 0; i<=10000; ++i){
+        if((a**2 + b**2 ) == (c**2)){
+            console.log(a,b,c);
+        }
+    ++b
+    ++c
+    }
+b=1
+c=2
+}
