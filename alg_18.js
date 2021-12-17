337bdd2e109c9823fde494a69eec598835fa88dc
marvindejesusrosariodelgado <rosariomalvin20@gmail.com>
2021-12-17 08:21:58 -0400

    Cambios preeliminares

--- alg_18.js
+++ alg_18.js
@@ -4,30 +4,20 @@ function tripoTriang(a, b, c) {
 
    if (a == b && b == c) {
       if (a == c) {
-       console.log(`El triangulo es Equilatero`);
-      }
- 
-   } else { 
-     if (a==b) {
-       console.log(`El triangulo es isosceles`);
-     } else {
-        if (b == c){
-          console.log(`El triangulo es isosceles`);
- 
-        } else {
-           if (a == c) {
-             console.log(`El triangulo es isosceles`);
- 
-           }
-        }
- 
-     }
- 
-      }
- 
- 
- 
-   }
- 
+         console.log(`El triangulo es Equilatero`);
+   } 
+   } else {
+      if (a==b||b==c) {
+         console.log(`El triangulo es isosceles`);
+       } else{
+          if (a == c) { 
+            console.log(`El triangulo es isosceles`);
+   
+          } 
+          }
+            }
 
-tripoTriang(8, 8, 8);
+                   
+                           }
+
+                           tripoTriang(10, 9, 10);
