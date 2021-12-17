7134ce3d784d38718377673e522351cc84384b91
marvindejesusrosariodelgado <rosariomalvin20@gmail.com>
2021-12-17 08:42:58 -0400

    Solucion de ejercicio 18

--- alg_18.js
+++ alg_18.js
@@ -1,23 +1,22 @@
+//declaracion de variables
+
+function tripoTriang(a, b, c){
+if (a == b && b == c) {
+ 
+   console.log("triangulo es Equilatero");
+
+} else if (a == b && b != c  ) {
+    console.log("triangulo isoseles");
+
+} else if(a!=b&&b==c ){
+    console.log("triangulo isoseles");
+    
+    
+}else {
+    console.log("triangulo escaleno");
+
+}
+}
+tripoTriang(10, 10, 10);
 
 
-function tripoTriang(a, b, c) {
-
-   if (a == b && b == c) {
-      if (a == c) {
-         console.log(`El triangulo es Equilatero`);
-   } 
-   } else {
-      if (a==b||b==c) {
-         console.log(`El triangulo es isosceles`);
-       } else{
-          if (a == c) { 
-            console.log(`El triangulo es isosceles`);
-   
-          } 
-          }
-            }
-
-                   
-                           }
-
-                           tripoTriang(10, 9, 10);
